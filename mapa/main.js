57823cb6a27f050f333506ae9d75cdf15cbfea07
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-04 08:27:43 -0300

    cambios mapa

--- mapa/main.js
+++ mapa/main.js
@@ -1,4 +1,5 @@
-
+window.onload = function(){
+	
 var contenedor = document.getElementById('popup');
 var contenido  = document.getElementById('popup-content');
 var cerrar	   = document.getElementById('popup-closer');
@@ -37,27 +38,31 @@ var map = new ol.Map({
 
 
 
-
 map.on('singleclick', function(evt){
+
 	var coordenada = evt.coordinate;
 	var hdms = ol.proj.transform(coordenada,'EPSG:3857', 'EPSG:4326');
-	contenido.innerHTML ="<a href='https://www.openstreetmap.org/#map=20/"+hdms[1]+"/"+hdms[0]+"&layers=H'>"+hdms[1]+"/"+hdms[0]+"</a>";
+	contenido.innerHTML =hdms[1]+"<br>"+hdms[0];
 	overlay.setPosition(coordenada);
 
+	
 	document.getElementById('coordenadas').innerHTML = hdms[1]+","+hdms[0];
-
-
+	document.getElementById('latitud_popup').value = hdms[0];
 
 					// crea marcador
 					var a = new ol.Feature({
-					        geometry:new ol.geom.Point(ol.proj.fromLonLat([-71.223970, -33.68105860])),
+					        geometry:new ol.geom.Point(ol.proj.fromLonLat(
+								[-71.223970 , -33.68105860]
+								)),
 					}); 
+					console.log(hdms[1] , hdms[0])
+					document.getElementById('latitud').value =  hdms[0];
 
 					a.setStyle(new ol.style.Style ({
 					    image: new ol.style.Icon({
 					        color:'black',
 					        corssOrigin:'anonymous',
-					        imgSize:[10,10],
+					        imgSize:[15,15],
 					        src:'https://sites.google.com/site/figuritasgeometricas/_/rsrc/1339206194345/circulo/circulo.jpg?height=344&width=375',
 
 					    })
@@ -80,3 +85,6 @@ map.on('singleclick', function(evt){
 
 
 })
+
+
+}

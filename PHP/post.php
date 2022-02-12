6b09fe34091c8e2752ce43d37b7c44b95a49063e
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-12 13:32:11 -0300

    prueba funciones swc

--- PHP/post.php
+++ PHP/post.php
@@ -10,7 +10,7 @@ $humedad_1500kPa        = humedad_1500kPa($arena , $arcilla , $m_organica);
 $humedad_33kPa          = humedad_33kPa($arena , $arcilla , $m_organica);
 
 $h_sat_33kPa            = humedadSaturada_33kPa($arena , $arcilla , $m_organica);
-echo humedadSaturada_0kPa($arena, $arcilla , $m_organica);
+$h_sat_0kPa             = humedadSaturada_0kPa($arena, $arcilla , $m_organica);
 
-echo json_encode(" h 1500:".$humedad_1500kPa.","." h 33  :".$humedad_33kPa.","." h sat 33:".$h_sat_33kPa);
+echo json_encode(" h 1500:".$humedad_1500kPa.","." h 33  :".$humedad_33kPa.","." h sat 33:".$h_sat_33kPa.", h sat 0".$h_sat_0kPa);
 ?>

bbe03448d5377395ecacf7aae1318ed6ecc5a8bc
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-12 11:00:08 -0300

    prueba backend

--- PHP/swc.php
+++ PHP/swc.php
@@ -3,16 +3,23 @@
 function humedad_1500kPa($S , $C , $OM){
     $u1500t = -0.024 * $S + 0.487 * $C + 0.006 * $OM + 0.005*($S * $OM) - 0.013*($C * $OM) + 0.068*($S * $C) + 0.031;
     $u1500 = $u1500t + (0.14 * $u1500t - 0.02);
-
     return $u1500;
-
+}
+/*
 function humedad_33kPa($S , $C , $OM){
   $u33t = -0.251*$S + 0.195*$C + 0.011*$OM + 0.006*($S * $OM) - 0.027*($C * $OM) + 0.452*($S * $C) + 0.299;
   $u33  = $u33t + (1.283*($u33t)**2 - 0.374*($u33t) - 0.015);
-
   return $u33; # fraccion %V de 0 a 1
-
-
+}
+function humedadSaturada_33kPa($S , $C , $OM){
+  $uSat_33t = 0.278 * $S + 0.034*$C + 0.022 * $OM - 0.018*($S * $OM) - 0.027*($C * $OM) - 0.584*($S * $C) + 0.078;
+  $uSat_33  = $uSat_33t + (0.636 * $uSat_33t - 0.107);
+  return $uSat_33;
+}
+function humedadSaturada_0kPa($S , $C , $OM){
+  return humedadSaturada_33kPa($S , $C , $OM) + humedad_33kPa($S , $C , $OM) -  0.097* $S + 0.043;
+}
+*/
 function swc(){
  return "hola";
 }

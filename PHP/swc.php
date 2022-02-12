5cb816dfee3c1ea71ab2075cf3916361beea461f
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-12 18:42:46 -0300

    prueba funciones swc

--- PHP/swc.php
+++ PHP/swc.php
@@ -96,19 +96,19 @@ function humedad_1500kPaAjustada_Ec($S , $C , $OM , $DF , $EC){
   $humedad = 0;
   $t = 0;
   $res =  array();
-  /*
+  
   while($humedad<1){
-    
+  /*  
     if($humedad!=0){
       $t = $a*($humedad**(-$b)) + ($uSat/ $humedad) * 32 *$EC;
     }
     if(1499.9 < $t And $t <1500){
       array_push($res ,(round($humedad,3)) );
     }
-    
+    */
     $humedad+=0.000001;
   }
-  */
+  
   return $res;//min($res);
 }
 

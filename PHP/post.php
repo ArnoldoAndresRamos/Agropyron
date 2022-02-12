1b1905a33c23d7045e700f8bb78e766cfc59a75b
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-12 07:03:45 -0300

    conexion backend

--- PHP/post.php
+++ PHP/post.php
@@ -5,5 +5,5 @@ $arcilla    = $_POST['arcilla'];
 $m_organica = $_POST['m_organica'];
 
 //echo json_encode('arena :'.$arena."<br>".'arcilla:'.$arcilla."<br>".'M.O:'.$m_organica;
-echo "sdkjfkljdf";
+echo "sdkjfkljdf".$arena;
 ?>

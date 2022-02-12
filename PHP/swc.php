0b9871845f715f2c351c9329e69b22fd4b441a00
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-12 15:54:44 -0300

    prueba funciones swc

--- PHP/swc.php
+++ PHP/swc.php
@@ -5,38 +5,32 @@ function humedad_1500kPa($S , $C , $OM){
     $u1500 = $u1500t + (0.14 * $u1500t - 0.02);
     return $u1500;
 }
-
 function humedad_33kPa($S , $C , $OM){
   $u33t = -0.251*$S + 0.195*$C + 0.011*$OM + 0.006*($S * $OM) - 0.027*($C * $OM) + 0.452*($S * $C) + 0.299;
   $u33  = $u33t + (1.283*($u33t)**2 - 0.374*($u33t) - 0.015);
   return $u33; 
 }
-
 function humedadSaturada_33kPa($S , $C , $OM){
   $uSat_33t = 0.278 * $S + 0.034*$C + 0.022 * $OM - 0.018*($S * $OM) - 0.027*($C * $OM) - 0.584*($S * $C) + 0.078;
   $uSat_33  = $uSat_33t + (0.636 * $uSat_33t - 0.107);
   return $uSat_33;
 }
-
 function humedadSaturada_0kPa($S , $C , $OM){
   return humedadSaturada_33kPa($S , $C , $OM) + humedad_33kPa($S , $C , $OM) -  0.097* $S + 0.043;
 }
-
 function densidadNormal_gcm3($S , $C , $OM){
     return (1 - humedadSaturada_0kPa($S , $C , $OM) ) *2.65;
 }
+
 
 
 /* EFECTO DE LA DENSIDAD */
 function densidadAjustada_gcm3($S , $C , $OM , $DF=1){
     return densidadNormal_gcm3($S , $C , $OM)*$DF;
 }
-
 function humedadSaturada_0kPaAjustadaDensidad($S , $C , $OM , $DF){
     return 1 - densidadAjustada_gcm3($S , $C , $OM ,$DF )/ 2.65;
 }
-
-
 function humedad_33kPaAjustadaDensidad($S , $C , $OM , $DF){
     return humedad_33kPa($S , $C , $OM) -0.2 * ( humedadSaturada_0kPa($S , $C , $OM) - humedadSaturada_0kPaAjustadaDensidad($S , $C , $OM , $DF) );
 }
@@ -44,7 +38,14 @@ function humedad_33kPaAjustadaDensidad($S , $C , $OM , $DF){
 function humedadSaturada_33kPaAjustadaDensidad($S , $C , $OM , $DF){
     return humedadSaturada_0kPaAjustadaDensidad($S , $C , $OM , $DF) - humedad_33kPaAjustadaDensidad($S , $C , $OM , $DF);
 }
-echo humedadSaturada_33kPaAjustadaDensidad(0.8,0.04,2.08,$DF=1.2);
+
+
+/* EFECTO DE LA GRAVA */
+function fraccionDeVolumenDeGrava($S,$C,$OM,$DF,$RW){
+    $alfa = densidadAjustada_gcm3($S,$C,$OM,$DF) / 2.65;
+    return (1-$RW) / (1-$RW * (1-1.5*$alfa));
+}
+echo fraccionDeVolumenDeGrava(0.8 , 0.04 , 2.08 , $DF=1.2 , $RW=0.1);
 function swc(){
  return "hola";
 }

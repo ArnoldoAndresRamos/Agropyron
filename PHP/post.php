fc54d5ab3e123d2204b25b661d53cdd1e99f3b63
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-12 06:54:09 -0300

    conexion backend

--- PHP/post.php
+++ PHP/post.php
@@ -3,16 +3,6 @@ $arena      = $_POST['arena'];
 $arcilla    = $_POST['arcilla'];
 $m_organica = $_POST['m_organica'];
 
+echo json_encode('arena :'.$arena."<br>".'arcilla:'.$arcilla."<br>".'M.O:'.$m_organica;
 
-$latitud     = $_POST['latitud'];
-$longitud    = $_POST['longitud'];
-
-
-
-if($temperatura_max ==='' || $temperatura_min ===''){
-    echo json_encode('error');
-}else{
-    //cho json_encode('usuario: '.$temperatura_max.'<br> pass: '.$temperatura_min);
-    echo json_encode('arena :'.$arena."<br>".'arcilla:'.$arcilla."<br>".'M.O:'.$m_organica;
-}
 ?>

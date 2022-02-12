1e45d279ff7b53f448b601e19f9efa09fae3b1d3
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-12 20:10:46 -0300

    prueba funciones swc

--- PHP/swc.php
+++ PHP/swc.php
@@ -140,7 +140,7 @@ function aguaDisponibleAjustada_CE($S , $C , $OM , $DF , $CE){
   */
 
 
-  /*
+  
 
 function SWC($S  , $C , $OM , $DF=1, $RW =0, $CE=0){
  
@@ -159,7 +159,7 @@ function SWC($S  , $C , $OM , $DF=1, $RW =0, $CE=0){
 }  
 echo SWC( $S=0.7 , $C=0.21 , $OM=2.5 ,$DF= 1.0 ,$RW=0, $CE=2.5);
 echo SWC( $S=0.7 , $C=0.21 , $OM=2.5 );
-*/
+
 
 
 ?>

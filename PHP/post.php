9d63ffd32c0a1fdd0f1d5f579edf7c762649ccee
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-12 11:03:20 -0300

    prueba backend

--- PHP/post.php
+++ PHP/post.php
@@ -7,7 +7,8 @@ $m_organica = $_POST['m_organica'];
 $j=swc();
 
 $humedad_1500kPa = humedad_1500kPa($arena , $arcilla , $m_organica);
+$humedad_33kPa   = humedad_33kPa($arena , $arcilla , $m_organica);
 
-echo json_encode('arena :'.$arena."<br>".'arcilla:'.$arcilla."<br>".'M.O:'.$m_organica." func".$humedad_1500kPa);
+echo json_encode('arena :'.$arena."<br>".'arcilla:'.$arcilla."<br>".'M.O:'.$m_organica." func".$humedad_1500kPa." h_33:".$humedad_33kPa);
 //echo json_encode("sdkjfkljdf".$arena);
 ?>

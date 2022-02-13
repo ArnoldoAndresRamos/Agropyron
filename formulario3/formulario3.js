94780956ea0433e8af5151323f0725323471814f
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-13 17:02:41 -0300

    formulario3

--- formulario3/formulario3.js
+++ formulario3/formulario3.js
@@ -4,7 +4,7 @@ function sendRequest1(){
 	
  	let b = new XMLHttpRequest()
     let url='https://aara.duckdns.org/agronono/formulario3/formulario3.php'
- 	b.open('POST',url,false);
+ 	b.open('POST',url,true);
  	b.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   	b.onreadystatechange = function(){
       

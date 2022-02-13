f67c6a3b84b8b0ce0803a2ab0554918764b11599
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-13 14:48:53 -0300

    formulario3

--- formulario3/formulario3.php
+++ formulario3/formulario3.php
@@ -1,6 +1,8 @@
 <?php
+include('./PHP/swc.php');
+$n = soil_water_characteristics(0.8,0.04,2.08);
 $s=$_POST['s'];
 $m=$_POST['m'];
 //echo "string"+$s+$m+34;
-echo "aksjhdajd".$s;
+echo $s.$m.", ".$n;
 ?>

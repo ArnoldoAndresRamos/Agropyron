74920676b07afa442a20a7b2aa81e99534f70a55
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-13 03:37:58 -0300

    prueba backend

--- PHP/post.php
+++ PHP/post.php
@@ -9,14 +9,6 @@ $res = soil_water_characteristics( $arena  , $arcilla , $m_organica);
 //echo $res;
 echo soil_water_characteristics( $S=0.2  , $C=0.2 , $OM=2.5 , $DF=1, $RW =0, $CE=0);
 
-/*
-$humedad_1500kPa        = humedad_1500kPa($arena , $arcilla , $m_organica);
-$humedad_33kPa          = humedad_33kPa($arena , $arcilla , $m_organica);
+echo json_encode("{ h 1500:".$humedad_1500kPa.","." h 33  :".$humedad_33kPa.","." h sat 33:".$h_sat_33kPa.", h sat 0:".$h_sat_0kPa.}", d Ajus:".$densidadNormal);
 
-$h_sat_33kPa            = humedadSaturada_33kPa($arena , $arcilla , $m_organica);
-$h_sat_0kPa             = humedadSaturada_0kPa($arena, $arcilla , $m_organica);
-$densidadNormal         = densidadNormal_gcm3($arena , $arcilla , $m_organica);
-
-echo json_encode(" h 1500:".$humedad_1500kPa.","." h 33  :".$humedad_33kPa.","." h sat 33:".$h_sat_33kPa.", h sat 0:".$h_sat_0kPa.", d Ajus:".$densidadNormal);
-*/
 ?>

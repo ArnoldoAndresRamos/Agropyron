f1bb36ae0e946efbe7f2767db6ec833bb1cc0dce
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-14 00:50:36 -0300

    formulario3

--- formulario3/formulario3.php
+++ formulario3/formulario3.php
@@ -4,5 +4,7 @@ $n = soil_water_characteristics(0.8,0.04,2.08);
 $s = $_POST['s'];
 $m = $_POST['m'];
 //echo "string"+$s+$m+34;
-echo json_encode($n);
+$datos = json_encode($n);
+//echo json_encode($n);
+$output = file_put_contents("output.json","w+",$datos);
 ?>

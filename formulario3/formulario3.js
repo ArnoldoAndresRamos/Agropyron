0b58b18072df0606f43a8377a65d5671e6427fbf
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-14 03:01:32 -0300

    pinrtando en pantalla ajax json

--- formulario3/formulario3.js
+++ formulario3/formulario3.js
@@ -16,6 +16,7 @@ function sendRequest1(){
             res.innerHTML+=`
             <tr>
                 <td>${datos[property]}</td>
+                <td>${property}</td>
             </tr>
             `
           }

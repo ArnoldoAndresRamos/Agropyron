64ffe338c5279a9d37b5c46d0b11b62be16c4808
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-14 03:51:04 -0300

    fetch en app.js

--- JS/app.js
+++ JS/app.js
@@ -1,40 +1,29 @@
-
 var formulario = document.getElementById('swc');
 var respuesta  = document.getElementById('respuesta');
 
 formulario.addEventListener('submit', function(e){
     e.preventDefault();
     var datos = new FormData(formulario);
-    console.log(JSON.parse(datos))
 
     fetch('PHP/post.php', {
         method:'POST',
         body:datos
         })  
     
-    .then(res=>res.text())
+    .then(res=>res.json())
     .then(data=>{
-        respuesta.innerHTML= `
-        <div class="alert alert-primary" role="alert">
-            ${data}
-        </div>
-        `
+        if(data ==='error'){ 
+            respuesta.innerHTML= `
+            <div class="alert alert-danger" role="alert">
+                A simple danger alert—check it out!
+            </div
+            `
+        }else{
+            respuesta.innerHTML= `
+            <div class="alert alert-primary" role="alert">
+                ${data}
+            </div
+            `
         }
       })
  })
- 
-/*
-function SWC(){
-    fetch('PHP/post.php')
-    .then(function(res){
-        console.log(res);
-        return res.json();
-    })
-    .then(function(data){
-        data=JSON.parse(data);
-        console.log(data);
-        
-        document.getElementById('respuesta').innerHTML=data['cc'];
-    })
-}
-*/

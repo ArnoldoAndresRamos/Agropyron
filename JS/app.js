455431911eb0fb842714f037f2be5a8576cf982c
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-14 04:02:19 -0300

    fetch en app.js

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,4 @@
-var formulario = document.getElementById('swc');
+var formulario = document.getElementById('formulario');
 var respuesta  = document.getElementById('respuesta');
 
 formulario.addEventListener('submit', function(e){
--- PHP/post.php
+++ PHP/post.php
@@ -5,9 +5,12 @@ $arena      = $_POST['arena']/100;
 $arcilla    = $_POST['arcilla']/100;
 $m_organica = $_POST['m_organica'];
 
-
+echo json_encode("arena:".$arena.","."arcilla".$arcilla);
+/*
 $suelo=soil_water_characteristics($arena,$arcilla,$m_organica);
 
 $age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
 echo json_encode($age);
+//echo json_encode('{"capacided de campo:"'.$suelo["cc"].',punto de marchitez permanente:'.$suelo["pmp"].',c:'.$suelo["sat"].',d:'.$suelo["aguaDisp"].',e:'.$suelo["ksat"].',f:'.$suelo["densidad"]."}");
+*/
 ?>

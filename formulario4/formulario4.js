6bed92f355a0ac7f09613e0129e84303f1da3b5e
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-20 15:34:21 -0300

    formulario4 ajax

--- formulario4/formulario4.js
+++ formulario4/formulario4.js
@@ -5,12 +5,12 @@ function send_formulario4(){
     let url='https://aara.duckdns.org/agronono/formulario4/formulario4.php'
     formulario4.open("POST",url);
 
-    formulario4.send("dato="+dato);
     
+
     formulario4.onreadystatechange = function(){
         var data = formulario4.responseText;
         document.getElementById('respuesta4').innerHTML = data;
     }
     
-
+    formulario4.send("dato="+dato);
 }

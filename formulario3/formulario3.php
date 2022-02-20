26e8056b12cc6c0b81514cd42a07e262910075eb
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-20 13:54:33 -0300

    formulario3

--- formulario3/formulario3.php
+++ formulario3/formulario3.php
@@ -8,7 +8,7 @@ $n = soil_water_characteristics(0.8,0.04,2.08);
 $s = $_POST['s'];
 $m = $_POST['m'];
 //echo "string"+$s+$m+34;
-$datos = json_encode($m);
+$datos = json_encode($n);
 echo $datos;
 
 // para modificar de beben tener los permisos

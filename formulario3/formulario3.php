2541067df2748c264f7c9f99189a360f858059cc
ArnoldoAndresRamos <andresarnoldo@live.com>
2022-02-20 14:44:57 -0300

    formulario3

--- formulario3/formulario3.php
+++ formulario3/formulario3.php
@@ -1,6 +1,6 @@
 <?php
 include('../PHP/swc.php');
-$arena      = $_POST['are1'];
+//$arena      = $_POST['are1'];
 //$arcilla    = $_POST['arcilla']/100;
 //$m_organica = $_POST['m_organica'];
 $n = soil_water_characteristics(0.8,0.04,2.08);
@@ -8,9 +8,9 @@ $n = soil_water_characteristics(0.8,0.04,2.08);
 $s = $_POST['s'];
 $m = $_POST['m'];
 //echo "string"+$s+$m+34;
-$datos = json_encode($arena);
+//$datos = json_encode($arena);
 //echo $datos;
-echo $m;
+echo $s;
 // para modificar de beben tener los permisos
 $myfile = fopen("output.json", "w"); 
 $bytes = fwrite($myfile, $datos); 
